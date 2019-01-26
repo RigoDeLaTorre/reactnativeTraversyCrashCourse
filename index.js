5c3322266d01386ee8c5c3157dfd3f50938a0c5c
Rigo De La Torre <rigoddd@gmail.com>
2019-01-26 14:35:13 -0800

    component5 done, listview with fetching data

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 /** @format */
 import React from 'react'
 import { View, Text, AppRegistry } from 'react-native'
-import Component4 from './src/components/Component4/Component4'
+import Component5 from './src/components/Component5/Component5'
 // import App from './App';
 // import {name as appName} from './app.json';
 const App = () => (
 	<View>
-		<Component4 />
+		<Component5 />
 	</View>
 )
 AppRegistry.registerComponent('AwesomeProject', () => App)
